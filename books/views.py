26497cfc51184b5a56bf8471fc6efe3cb78f94be
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-27 15:22:03 +0200

    Implement Book API with ViewSet, Serializer, and routing

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
