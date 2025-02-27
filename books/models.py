1189c54c72f780228a50a527186fbe60ff34047d
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-27 15:00:46 +0200

    Implement Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", _("Hard")
+        SOFT = "SOFT", _("Soft")
+
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    cover = models.CharField(max_length=4, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    class Meta:
+        unique_together = ("title", "author")
+        ordering = ("title", "author")
