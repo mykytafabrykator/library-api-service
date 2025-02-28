336b5e5193ebf4c8ab4ab93dbf9dc8f75a9d29ac
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-28 15:52:01 +0200

    Feat: implement borrow_book() action with transaction handling

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,11 +1,15 @@
-from rest_framework import mixins
+from django.db import transaction
+from rest_framework import mixins, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingListSerializer,
-    BorrowingRetrieveSerializer
+    BorrowingRetrieveSerializer,
+    BorrowingCreateSerializer
 )
 
 
@@ -27,5 +31,26 @@ class BorrowingsViewSet(
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingRetrieveSerializer
+        if self.action == "borrow_book":
+            return BorrowingCreateSerializer
 
         return BorrowingListSerializer
+
+    @action(detail=False, methods=["POST"], url_path="borrow")
+    def borrow_book(self, request):
+        serializer = self.get_serializer(
+            data=request.data,
+            context={"request": request}
+        )
+
+        if serializer.is_valid():
+            with transaction.atomic():
+                book = serializer.validated_data["book"]
+                book.inventory -= 1
+                book.save(update_fields=["inventory"])
+
+                serializer.save(user=request.user)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
