2f35bb2963f56313edba4297ef464eaedc131308
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-28 15:51:22 +0200

    Feat: add BorrowingCreateSerializer with validation

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -23,3 +23,26 @@ class BorrowingListSerializer(serializers.ModelSerializer):
 
 class BorrowingRetrieveSerializer(BorrowingListSerializer):
     book = BookSerializer(read_only=True)
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "borrow_date", "expected_return_date")
+
+    def validate(self, data):
+        book = data.get("book")
+        borrow_date = data.get("borrow_date")
+        expected_return_date = data.get("expected_return_date")
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError(
+                {"book": "This book is not available for borrowing."}
+            )
+
+        Borrowing.validate_borrowing(
+            borrow_date=borrow_date,
+            expected_return_date=expected_return_date,
+        )
+
+        return data
