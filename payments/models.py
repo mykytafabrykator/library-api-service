8e799f080c956b63d62c8dc707ca2941b70dc319
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-02 20:27:51 +0200

    refactor: add money_to_pay field and calculation method in Payment model

--- payments/models.py
+++ payments/models.py
@@ -24,12 +24,13 @@ class Payment(models.Model):
     )
     session_url = models.URLField(blank=True, null=True)
     session_id = models.CharField(max_length=255, unique=True)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
 
-    @property
-    def money_to_pay(self) -> Decimal:
-        """Calculate the total cost of borrowing."""
-        borrowing = self.borrowing
-
+    @staticmethod
+    def calculate_money_to_pay(borrowing) -> Decimal:
+        """
+        Calculate the total amount to be paid for the borrowing.
+        """
         return_date = (borrowing.actual_return_date
                        or borrowing.expected_return_date)
         borrowing_days = max(1, (return_date - borrowing.borrow_date).days)
